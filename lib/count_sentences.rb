84113c62dc54f7e387eaf49e1d51d26a8064a17e
Morgan Collado <morgan.collado@gmail.com>
2020-05-21 23:56:10 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    count_sentences = self.split("?", "!").flatten
+    count_sentences = self.split((/[.?!] /)).flatten
     count_sentences.count
   end
 end
